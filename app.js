0a11e42fe7894fba60092eb13bdc471c1d51ff97
njang <neo.w.jang@gmail.com>
2018-01-09 15:12:21 -0600

    Style index.html

--- app.js
+++ app.js
@@ -27,6 +27,10 @@ app.post('/', function(req, res, next) {
  // Handle the post for this route
 });
 
+app.get('*', function(req, res, next) {
+	res.status(404).sendFile('error.html', {root: 'public'});
+});
+
 // Listening on port 3000. Node will display the following message if the server is running.
 app.listen(3000, ()=>{
     console.log("I am listening");
